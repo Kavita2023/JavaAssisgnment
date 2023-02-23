e1faaa452f330620a994e3227c88e168532efa5f
Kavita2023 <126144034+Kavita2023@users.noreply.github.com>
2023-02-23 13:43:43 +0530

    Add files via upload

---
+++ ExampleString1.java
@@ -0,0 +1,11 @@
+package com.ericsson.str;
+
+public class ExampleString1 {
+	public static void main(String[] args) {
+		String name = "ercisson";
+		char result = name.charAt(3);
+		
+		System.out.println("Character define in index number : " + result);
+	}
+
+}
