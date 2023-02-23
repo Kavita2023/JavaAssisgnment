98516ddbc0854de4ce57b29f7a4de862579c4b40
Kavita2023 <126144034+Kavita2023@users.noreply.github.com>
2023-02-23 13:44:50 +0530

    Add files via upload

---
+++ ExampleString6.java
@@ -0,0 +1,14 @@
+package com.ericsson.str;
+
+public class ExampleString6 {
+	public static void main(String[] args) {
+		String name = "ercisson";
+		//String name2 = "telsson";
+		
+		
+		System.out.println(name.toUpperCase());
+		//System.out.println(name2.length());
+	}
+
+
+}
