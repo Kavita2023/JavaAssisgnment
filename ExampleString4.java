78829400d316a58764e86f442fcf0a9ea6028f20
Kavita2023 <126144034+Kavita2023@users.noreply.github.com>
2023-02-23 13:44:23 +0530

    Add files via upload

---
+++ ExampleString4.java
@@ -0,0 +1,12 @@
+package com.ericsson.str;
+
+public class ExampleString4 {
+	public static void main(String[] args) {
+		String name = "ercisson";
+		String name2 = "telsson";
+		
+		
+		System.out.println(name.endsWith("sson"));
+		System.out.println(name2.endsWith("eon"));
+	}
+}
