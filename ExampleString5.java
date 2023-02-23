24abf03a85e480c32a53cd5e1beddbd7a60eced1
Kavita2023 <126144034+Kavita2023@users.noreply.github.com>
2023-02-23 13:44:34 +0530

    Add files via upload

---
+++ ExampleString5.java
@@ -0,0 +1,13 @@
+package com.ericsson.str;
+
+public class ExampleString5 {
+	public static void main(String[] args) {
+		String name = "ercisson";
+		String name2 = "telsson";
+		
+		
+		System.out.println(name.length());
+		System.out.println(name2.length());
+	}
+
+}
