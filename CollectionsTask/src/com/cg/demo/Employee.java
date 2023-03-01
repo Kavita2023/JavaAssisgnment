9dbe6c4402eb8c23f95816cbe5f32d553d7390a9
Kavita2023 <126144034+Kavita2023@users.noreply.github.com>
2023-03-01 09:40:01 +0530

    Update Employee.java

--- CollectionsTask/src/com/cg/demo/Employee.java
+++ CollectionsTask/src/com/cg/demo/Employee.java
@@ -21,6 +21,52 @@ public class Employee {
 		this.location = location;
 	}
 
-	//generate setters, getters and toString
+	// generate setters, getters and toString
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", designation=" + designation
+				+ ", location=" + location + "]";
+	}
 
 }
